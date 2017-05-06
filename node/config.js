f7c6b3075df74d588f988dc3d19d8534e6409432
Vladislav Druzhbinskiy <studio_kseven@mail.ru>
2017-05-06 20:47:50 +0300

    1.1.22

--- node/config.js
+++ node/config.js
@@ -1,18 +1,28 @@
 let config;
 module.exports = config  = {
+	port: {
+		dev: 8080,
+		prod: 80
+	},
 	urlPaths: {
 		dev: "http://localhost:8090",
-		prod: "http://198.211.121.46:8090"
+		prod: "http://198.211.121.46:8080"
 	},
 	urlServerPath: {
 		dev: "http://localhost:8080",
-		prod: "http://198.211.121.46"
+		prod: "http://198.211.121.46:8080"
 	},
-	sqlDatas: {
+	sqlDatasDev: {
 		host: "localhost",
 		user: "root",
 		password: "root",
 		database: "animals_db"
+	},
+	sqlDatasProd: {
+		host: "http://198.211.121.46",
+		user: "root",
+		password: "bmw1forme",
+		database: "animalsDataBase"
 	},
 	secret: "dradnout"
 };
