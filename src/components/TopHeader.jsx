08a946a47bc2fbbc84bf9fd569bae43d0a451b3f
Vladislav Druzhbinskiy <studio_kseven@mail.ru>
2017-05-14 23:39:10 +0300

    1.1.45

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -46,7 +46,7 @@ class TopHeader extends Component {
 						<Link to="/">
 							<img src="uploads/logo2.png" width="120" alt="logotype" />
 						</Link>
-						<h1 className="h1">Продай, купи, найди, отдай в дар или размести объявление о пропаже любого животного.</h1>
+						<h1 className="h1">Продай, купи, отдай в дар, сообщи о находке или размести объявление о пропаже любого животного.</h1>
 					</div>
 					<LoginModal titleBtn={this.props.state.filterCity.cityTopHeader} dialogModal="02" />
 					{
