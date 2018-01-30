9c5c8b443abaf315cb5d6685d55a27ac321a99e8
Vladislav Druzhbinskiy <studio_kseven@mail.ru>
2018-01-30 23:13:26 +0300

    ref spa 04

--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute } from 'react-router'
 
-import App from './App.js'
-import { store, history } from './store'
-import Conf from './components/conf/ConfComponent'
-import NotFound from './components/notFound/NotFoundComponent'
-import Moderate from './components/moderate/ModerateComponent'
-import PersonalArea from './components/personalArea/PersonalAreaComponent'
-import AddCardFormComponent from './components/forms/addCardForm/AddCardFormComponent'
-import ContactsFormComponent from './components/forms/contactsForm/ContactsFormComponent'
-import WrapAnimalCard from './components/categories/wrapAnimalCard/WrapAnimalCardComponent'
+import routing from './routing'
 
-const onEnterFunc = (nextState, replaceState) => {
-	if (!store.getState().loginUser || store.getState().loginUser.error) replaceState('/')
-}
-
-ReactDOM.render(
-	<Provider store={ store }>
-		<Router history={ history }>
-			<Route path='/' component={ App }>
-				<Route path='/conf' component={ Conf } />
-				<IndexRoute component={ WrapAnimalCard } />
-				<Route path='/contacts' component={ ContactsFormComponent } />
-				<Route path='/animals/:type/:advertisment' component={ WrapAnimalCard }/>
-				<Route path='/moderation' component={ Moderate } onEnter={ onEnterFunc } />
-				<Route path='/personalArea' component={ PersonalArea } onEnter={ onEnterFunc } />
-				<Route path='/placeAnAd' component={ AddCardFormComponent } onEnter={ onEnterFunc } />
-			</Route>
-			<Route path='*' component={ NotFound } />
-		</Router>
-	</Provider>,
-	document.getElementById('app')
-)
+ReactDOM.render(routing, document.getElementById('app'))
---
+++ client/src/routing.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute } from 'react-router'
+
+import App from './App.js'
+import { store, history } from './store'
+
+import Conf from './components/conf/ConfComponent'
+import NotFound from './components/notFound/NotFoundComponent'
+import Moderate from './components/moderate/ModerateComponent'
+import PersonalArea from './components/personalArea/PersonalAreaComponent'
+import AddCardFormComponent from './components/forms/addCardForm/AddCardFormComponent'
+import ContactsFormComponent from './components/forms/contactsForm/ContactsFormComponent'
+import WrapAnimalCard from './components/categories/wrapAnimalCard/WrapAnimalCardComponent'
+
+const onEnterFunc = (nextState, replaceState) => {
+	if (!store.getState().loginUser || store.getState().loginUser.error) replaceState('/')
+}
+
+const routing = <Provider store={ store }>
+	<Router history={ history }>
+		<Route path='/' component={ App }>
+			<Route path='/conf' component={ Conf } />
+			<IndexRoute component={ WrapAnimalCard } />
+			<Route path='/contacts' component={ ContactsFormComponent } />
+			<Route path='/animals/:type/:advertisment' component={ WrapAnimalCard }/>
+			<Route path='/moderation' component={ Moderate } onEnter={ onEnterFunc } />
+			<Route path='/personalArea' component={ PersonalArea } onEnter={ onEnterFunc } />
+			<Route path='/placeAnAd' component={ AddCardFormComponent } onEnter={ onEnterFunc } />
+		</Route>
+		<Route path='*' component={ NotFound } />
+	</Router>
+</Provider>
+
+export default routing
