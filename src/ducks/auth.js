2ff5d0c33b05711b4b58510c63b27c05e3af0bbe
Vladislav D <studio_kseven@mail.ru>
2019-03-06 15:17:44 +0300

    add get auth token

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -16,17 +16,17 @@ const ReducerSchema = Record({
 })
 
 export const types = {
-	SIGN_UP_REQUEST: `${ appName }/${ moduleName }/SIGN_UP_REQUEST`,
-	SIGN_UP_SUCCESS: `${ appName }/${ moduleName }/SIGN_UP_SUCCESS`,
-	SIGN_UP_ERROR: `${ appName }/${ moduleName }/SIGN_UP_ERROR`,
+	AUTH_REQUEST: `${ appName }/${ moduleName }/AUTH_REQUEST`,
+	AUTH_SUCCESS: `${ appName }/${ moduleName }/AUTH_SUCCESS`,
+	AUTH_ERROR: `${ appName }/${ moduleName }/AUTH_ERROR`,
 
-	SIGN_IN_REQUEST: `${ appName }/${ moduleName }/SIGN_IN_REQUEST`,
-	SIGN_IN_SUCCESS: `${ appName }/${ moduleName }/SIGN_IN_SUCCESS`,
-	SIGN_IN_ERROR: `${ appName }/${ moduleName }/SIGN_IN_ERROR`,
+	REGISTRATION_REQUEST: `${ appName }/${ moduleName }/REGISTRATION_REQUEST`,
+	REGISTRATION_SUCCESS: `${ appName }/${ moduleName }/REGISTRATION_SUCCESS`,
+	REGISTRATION_ERROR: `${ appName }/${ moduleName }/REGISTRATION_ERROR`,
 
-	SIGN_OUT_REQUEST: `${ appName }/${ moduleName }/SIGN_OUT_REQUEST`,
-	SIGN_OUT_SUCCESS: `${ appName }/${ moduleName }/SIGN_OUT_SUCCESS`,
-	SIGN_OUT_ERROR: `${ appName }/${ moduleName }/SIGN_OUT_ERROR`
+	LOGOUT_REQUEST: `${ appName }/${ moduleName }/LOGOUT_REQUEST`,
+	LOGOUT_SUCCESS: `${ appName }/${ moduleName }/LOGOUT_SUCCESS`,
+	LOGOUT_ERROR: `${ appName }/${ moduleName }/LOGOUT_ERROR`
 }
 
 export const actions = {
@@ -37,7 +37,7 @@ export const actions = {
 	},
 
 	loginAction: ({ email, password }) => dispatch => {
-		dispatch({ type: types.SIGN_IN_REQUEST })
+		dispatch({ type: types.AUTH_REQUEST })
 
 		fetch(`${ config.payPetsApiUrl }/api/auth/login`, {
 			headers: {
@@ -46,13 +46,22 @@ export const actions = {
 			method: 'POST',
 			body: JSON.stringify({ email, password })
 		})
-			.then(result => result.json())
+			.then(response => {
+				if (response.ok) return response.json()
+				return Promise.reject(response.json())
+			})
 			.then(user => {
 				const { token } = user
 
 				localStorage.setItem('token', token)
+				dispatch({ type: types.AUTH_SUCCESS, user })
 			})
-			.catch(err => dispatch(actionsSnackbarReducer.handleSnackbar(err)))
+			.catch(err => {
+				err.then(res => {
+					dispatch(actionsSnackbarReducer.handleSnackbar(res.message))
+					dispatch({ type: types.AUTH_ERROR })
+				})
+			})
 	},
 
 	signUp: ({ email, password, name, surName, city, phoneNumber }) => dispatch => {
@@ -70,17 +79,27 @@ export default (state = new ReducerSchema(), action) => {
 	const { type, payload, userDatas, error } = action
 
 	switch (type) {
-	case types.SIGN_UP_REQUEST: return state.set('userLoading', true).set('userError', false).set('user', null)
-	case types.SIGN_UP_SUCCESS: return state.set('userLoading', false).set('userError', false).set('user', payload)
-	case types.SIGN_UP_ERROR: return state.set('userError', error).set('userLoading', false)
+	case types.AUTH_REQUEST: return {
+		userLoading: true,
+		userError: false
+	}
+	case types.AUTH_SUCCESS: return {
+		userLoading: false,
+		userError: false,
+		user: action.user
+	}
+	case types.AUTH_ERROR: return {
+		userError: true,
+		userLoading: false
+	}
 
-	case types.SIGN_IN_REQUEST: return state.set('userLoading', true).set('userError', false)
-	case types.SIGN_IN_SUCCESS: return state.set('userLoading', false).set('userError', false).set('user', payload).set('userDatas', userDatas)
-	case types.SIGN_IN_ERROR: return state.set('userError', error).set('userLoading', false)
+	case types.REGISTRATION_REQUEST: return state.set('userLoading', true).set('userError', false).set('user', null)
+	case types.REGISTRATION_SUCCESS: return state.set('userLoading', false).set('userError', false).set('user', payload)
+	case types.REGISTRATION_ERROR: return state.set('userError', error).set('userLoading', false)
 
-	case types.SIGN_OUT_REQUEST: return state.set('userLoading', true)
-	case types.SIGN_OUT_SUCCESS: return state.set('user', null).set('userError', null).set('userLoading', false).set('userDatas', null)
-	case types.SIGN_OUT_ERROR: return state.set('userError', error).set('userLoading', false)
+	case types.LOGOUT_REQUEST: return state.set('userLoading', true)
+	case types.LOGOUT_SUCCESS: return state.set('user', null).set('userError', null).set('userLoading', false).set('userDatas', null)
+	case types.LOGOUT_ERROR: return state.set('userError', error).set('userLoading', false)
 
 	default: return state
 	}
