37a6270377bed1437f1720f1c7b5bd621325c3ce
Vladislav D <studio_kseven@mail.ru>
2019-03-09 23:25:43 +0300

    update contact page

--- src/components/forms/contactsForm/ContactsFormComponent.js
+++ src/components/forms/contactsForm/ContactsFormComponent.js
@@ -1,147 +1,17 @@
-import { Link } from 'react-router'
-import classNames from 'classnames'
-import { connect } from 'react-redux'
-import React, { Component, PropTypes } from 'react'
-import { Form, Field, reduxForm } from 'redux-form'
-
-import { renderField, validate } from '../formValidate'
-import { normilizeText, validateInputs } from '../validationsInputs'
-
-import { actions as actionsContactFormStatus } from '../../../ducks/contactFormStatus'
+import React, { Component } from 'react'
 
 if (process.env.BROWSER) {
 	require('./ContactsStyles.sass')
 }
 
-class ContactsForm extends Component {
-
-	state = { disabledButton: true }
-
-	componentWillUpdate(nextProps) {
-		if (nextProps !== this.props) {
-			this.disabledSubmitButton(nextProps)
-		}
-	}
-
-	disabledSubmitButton = nextProps => {
-		const { contactsForm: { values } } = nextProps
-
-		if (values &&
-			values.name &&
-			values.email &&
-			values.title &&
-			values.textArea &&
-			values.check &&
-			values.name.match(validateInputs.name) &&
-			values.email.match(validateInputs.email) &&
-			values.title.match(validateInputs.title) &&
-			values.textArea.match(validateInputs.textArea)
-		) { this.setState({ disabledButton: false }) }
-		else { this.setState({ disabledButton: true }) }
-	}
-
-	handleMessage = event => {
-		event.preventDefault()
-
-		const { contactsForm: { values }, handleSnackbar } = this.props
-
-		fetch('https://us-central1-animals-bbfac.cloudfunctions.net/sendemail', {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			method: 'POST',
-			body: JSON.stringify(values)
-		})
-			.then(res => {
-				if (res.status === 200) {
-					handleSnackbar('Отправлено')
-				}
-			})
-			.catch(() => handleSnackbar('Ошибка отправки. Пожалуйста напишите мне в Вк'))
-	}
-
+export default class ContactsForm extends Component {
 	render() {
-		const style = { checkbox: { marginTop: '20px' } }
-
 		return (
 			<div className='contacts'>
-				<div className='formContacts'>
-					<p className='conTitle'>
-						Свяжитесь с нами по любым интересующим вас вопросам
-					</p>
-					<p><a href='https://vk.com/vladfebruary' target='_blank'>vk.com/vladfebruary</a></p>
-
-					<Form onSubmit={ this.handleMessage } className='sendForm'>
-						<Field
-							type='text'
-							label='Имя'
-							name='name'
-							normalize={ normilizeText }
-							component={ renderField }
-						/>
-						<Field
-							type='text'
-							label='Email'
-							name='email'
-							component={ renderField }
-						/>
-						<Field
-							type='text'
-							label='Тема сообщения'
-							name='title'
-							normalize={ normilizeText }
-							component={ renderField }
-						/>
-						<Field
-							type='text'
-							label='Сообщение'
-							name='textArea'
-							normalize={ normilizeText }
-							component={ renderField }
-						/>
-						<div>
-							<br />
-							<br />
-							<button
-								className={ classNames({
-									button2: true,
-									disabledButton: this.state.disabledButton
-								}) }
-								disabled={ this.state.disabledButton }
-								onClick={ this.handleMessage }
-							>
-								Отправить
-							</button>
-							<br />
-							<br />
-						</div>
-						<Field
-							type='checkbox'
-							label='Даю согласие на обработку персональных данных'
-							name='check'
-							component={ renderField }
-							className='checkBoxLink'
-							extra={ { style: style.checkbox } }
-						/>
-						<Link to='conf'>Политика конфиденциальности</Link>
-					</Form>
-				</div>
-
-				<div className='author'></div>
+				<p className='conTitle'>
+					По всем интересующим вас вопросам вы можете связаться с нами написав на email: studio_kseven@mail.ru
+				</p>
 			</div>
 		)
 	}
 }
-
-ContactsForm.propTypes = {
-	contactsForm: PropTypes.object,
-	handleSnackbar: PropTypes.func.isRequired
-}
-
-ContactsForm = reduxForm({
-	form: 'contactsForm',
-	validate
-})(ContactsForm)
-
-export default connect(
-	state => ({ contactsForm: state.form.contactsForm }),
-	{ ...actionsContactFormStatus }
-)(ContactsForm)
